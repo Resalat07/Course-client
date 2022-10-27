2b889a178bb88f42a3af7ddc1ba7a9b9f49d943f
Resalat07 <resalatefti@gmail.com>
2022-10-27 06:06:37 -0700

    blo2

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -28,21 +28,15 @@ const Blog = () => {
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
-                    <Accordion.Header> How does the private route work? #2</Accordion.Header>
+                    <Accordion.Header> How does the private route work? #3</Accordion.Header>
                     <Accordion.Body>
                     The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="3">
-                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Header>What is Node? How does Node work? #4</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
+                    It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
